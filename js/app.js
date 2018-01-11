39de048d3711d18805e0a8bcc0c82567aef4588f
david-dacruz <dvddcrz@gmail.com>
2018-01-11 22:18:31 +0100

    js added google search and show function and added tennis ball css for button

--- js/app.js
+++ js/app.js
@@ -1,19 +1,44 @@
-
-document.getElementById("hidden-container").classList.add("hidden");
-
-window.onload = function() {
+window.onload = function() {  
+    document.getElementById("hidden-container").classList.add("hidden");
     setTimeout(function() {
         sweetAlert({
-            title: "Le site xxxvidsxxx est trop bien. Viens dessus stp please", 
-            text: "Interdit au moins de 18 ans", 
-            type: "info",
+            title: "Interdit au moins de 18 ans", 
+            text: "Le site xxxvidsxxx est trop bien. Viens dessus stp please", 
+            //type: "info",
+            imageUrl: "./img/girl.png",
             showCancelButton: true,
             closeOnConfirm: false,
             confirmButtonText: "Yes, I am 18 or older!",
             confirmButtonColor: "#3cba54",
-            cancelButtonText: "Close",
+            cancelButtonText: "Close"
         });
   }, 10000);
+}
+
+// get the search box input by id       
+const searchBox = document.getElementById("searchBox");
+
+// add event listener to input
+searchBox.addEventListener('keyup', (e) => {
+
+    let userText = e.target.value;
+
+    if(userText == '')
+    {
+        userText.preventDefault;
+        console.log("empty search terms ...");
+    }
+    else{
+        userText.preventDefault;
+        let replaced = userText.split(' ').join('+');  
+        searchGoogle(replaced);
+    }
+});
+
+function searchGoogle(value){
+    document.getElementById("search-button").onclick = function () {
+        location.href = "https://www.google.fr/#q=" + value;
+    };
 }
 
 function show() {
@@ -21,3 +46,6 @@ function show() {
     document.getElementById("button-container").classList.add("hidden");
     container.style.display = "block";
 }
+
+  
+
